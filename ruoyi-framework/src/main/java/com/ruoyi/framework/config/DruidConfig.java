8c150d32c2ae6b60fd03f0753d96d4d740af6e85
zfguo <515830595@qq.com>
2024-01-29 16:55:03 +0800

    修复已知问题

--- ruoyi-framework/src/main/java/com/ruoyi/framework/config/DruidConfig.java
+++ ruoyi-framework/src/main/java/com/ruoyi/framework/config/DruidConfig.java
@@ -84,46 +84,46 @@ public class DruidConfig
     /**
      * 去除监控页面底部的广告
      */
-//    @SuppressWarnings({ "rawtypes", "unchecked" })
-//    @Bean
-//    @ConditionalOnProperty(name = "spring.datasource.druid.statViewServlet.enabled", havingValue = "true")
-//    public FilterRegistrationBean removeDruidFilterRegistrationBean(DruidStatProperties properties)
-//    {
-//        // 获取web监控页面的参数
-//        DruidStatProperties.StatViewServlet config = properties.getStatViewServlet();
-//        // 提取common.js的配置路径
-//        String pattern = config.getUrlPattern() != null ? config.getUrlPattern() : "/druid/*";
-//        String commonJsPattern = pattern.replaceAll("\\*", "js/common.js");
-//        final String filePath = "support/http/resources/js/common.js";
-//        // 创建filter进行过滤
-//        Filter filter = new Filter()
-//        {
-//            @Override
-//            public void init(jakarta.servlet.FilterConfig filterConfig) throws ServletException
-//            {
-//            }
-//            @Override
-//            public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-//                    throws IOException, ServletException
-//            {
-//                chain.doFilter(request, response);
-//                // 重置缓冲区，响应头不会被重置
-//                response.resetBuffer();
-//                // 获取common.js
-//                String text = Utils.readFromResource(filePath);
-//                // 正则替换banner, 除去底部的广告信息
-//                text = text.replaceAll("<a.*?banner\"></a><br/>", "");
-//                text = text.replaceAll("powered.*?shrek.wang</a>", "");
-//                response.getWriter().write(text);
-//            }
-//            @Override
-//            public void destroy()
-//            {
-//            }
-//        };
-//        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
-//        registrationBean.setFilter(filter);
-//        registrationBean.addUrlPatterns(commonJsPattern);
-//        return registrationBean;
-//    }
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    @Bean
+    @ConditionalOnProperty(name = "spring.datasource.druid.statViewServlet.enabled", havingValue = "true")
+    public FilterRegistrationBean removeDruidFilterRegistrationBean(DruidStatProperties properties)
+    {
+        // 获取web监控页面的参数
+        DruidStatProperties.StatViewServlet config = properties.getStatViewServlet();
+        // 提取common.js的配置路径
+        String pattern = config.getUrlPattern() != null ? config.getUrlPattern() : "/druid/*";
+        String commonJsPattern = pattern.replaceAll("\\*", "js/common.js");
+        final String filePath = "support/http/resources/js/common.js";
+        // 创建filter进行过滤
+        Filter filter = new Filter()
+        {
+            @Override
+            public void init(jakarta.servlet.FilterConfig filterConfig) throws ServletException
+            {
+            }
+            @Override
+            public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+                    throws IOException, ServletException
+            {
+                chain.doFilter(request, response);
+                // 重置缓冲区，响应头不会被重置
+                response.resetBuffer();
+                // 获取common.js
+                String text = Utils.readFromResource(filePath);
+                // 正则替换banner, 除去底部的广告信息
+                text = text.replaceAll("<a.*?banner\"></a><br/>", "");
+                text = text.replaceAll("powered.*?shrek.wang</a>", "");
+                response.getWriter().write(text);
+            }
+            @Override
+            public void destroy()
+            {
+            }
+        };
+        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+        registrationBean.setFilter(filter);
+        registrationBean.addUrlPatterns(commonJsPattern);
+        return registrationBean;
+    }
 }
--- ruoyi-framework/src/main/java/com/ruoyi/framework/config/SecurityConfig.java
+++ ruoyi-framework/src/main/java/com/ruoyi/framework/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -31,6 +32,7 @@ import static org.springframework.security.web.util.matcher.AntPathRequestMatche
  */
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig
 {
     /**
